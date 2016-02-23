61254b4248de89513a6760ff1936d6c2fa6dd8fc
碎碎酱 <yinxin630@gmail.com>
2016-02-23 18:33:51 +0800

    feat(body): add base body structure.

--- src/javascript/body.jsx
+++ src/javascript/body.jsx
@@ -12,20 +12,60 @@ export default class Body extends React.Component {
                 <div style={{
                     backgroundColor: 'aquamarine',
                     width: '10rem',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
                 }}>
-                    侧栏
+                    <div style={{
+                        width: '8rem',
+                        height: '8rem',
+                        backgroundColor: 'red',
+                    }}>
+                        头像
+                    </div>
+                    <span>昵称</span>
+                    <button>聊天</button>
+                    <button>论坛</button>
                 </div>
                 <div style={{
                     backgroundColor: 'cadetblue',
                     width: '20rem',
+                    display: 'flex',
+                    flexDirection: 'column',
                 }}>
-                    联系人
+                    <div style={{
+                        height: '6rem',
+                        backgroundColor: 'green',
+                    }}>联系人1</div>
+                    <div style={{
+                        height: '6rem',
+                        backgroundColor: 'blue',
+                    }}>联系人2</div>
                 </div>
                 <div style={{
                     backgroundColor: 'teal',
                     flex: 1,
+                    display: 'flex',
+                    flexDirection: 'column',
                 }}>
-                    聊天框
+                    <div style={{
+                        height: '4rem',
+                        backgroundColor: 'pink',
+                    }}>
+                        头部
+                    </div>
+                    <div style={{
+                        flex: 1,
+                        backgroundColor: 'purple',
+                    }}>
+                        主框体
+                    </div>
+                    <div style={{
+                        height: '6rem',
+                        backgroundColor: 'thistle',
+                    }}>
+                        输入区
+                    </div>
                 </div>
             </div>
         )
