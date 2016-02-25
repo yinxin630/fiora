c669fea2255c671ec41d2d91c13812be7e7e3a48
碎碎酱 <yinxin630@gmail.com>
2016-02-25 11:54:34 +0800

    feat(inputarea): add inputarea component

--- src/javascript/body.jsx
+++ src/javascript/body.jsx
@@ -8,6 +8,7 @@ import Sidebar from './sidebar.jsx';
 import Topbar from './topbar.jsx';
 import Message from './message.jsx';
 import ChatForm from './chatForm.jsx';
+import InputArea from './inputArea.jsx';
 
 export default class Body extends React.Component {
     render () {
@@ -15,7 +16,6 @@ export default class Body extends React.Component {
             <div style={{
                 flex: 1,
                 display: 'flex',
-                zIndex: -1,
             }}>
                 <Sidebar>
                     <User/>
@@ -41,11 +41,7 @@ export default class Body extends React.Component {
                         <Message/>
                         <Message align="right"/>
                     </ChatForm>
-                    <div style={{
-                        height: '6rem',
-                    }}>
-                        输入区
-                    </div>
+                    <InputArea/>
                 </div>
             </div>
         )
