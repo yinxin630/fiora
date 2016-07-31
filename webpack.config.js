e4da7c15808a8c96fa006d672443207eafa9bbac
碎碎酱 <yinxin630@gmail.com>
2016-07-31 13:24:51 +0800

    feat: add sag loader

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ module.exports = {
             {
                 test: /\.(png|jpg)$/,
                 loader: 'url-loader'
+            },
+            {
+                test: /\.svg/, 
+                loader: 'svg-url-loader'
             }
         ]
     },
