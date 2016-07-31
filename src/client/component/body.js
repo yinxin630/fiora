c37ec9e376c0b3504ac2b0261cfceb002c298e38
碎碎酱 <yinxin630@gmail.com>
2016-07-31 22:23:41 +0800

    refactor: add messageList component

--- src/client/component/body.js
+++ src/client/component/body.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../style/body.scss';
 
 import UserList from './userList';
+import MessageList from './messageList';
 
 class Body extends React.Component {
     render () {
@@ -29,56 +30,11 @@ class Body extends React.Component {
                             </div>
                         </div>
                     </div>
-                    <div className="content">
-                        <div className="message">
-                            <img src={ require('../image/avatar.gif') }/>
-                            <div>
-                                <div>
-                                    <span>碎碎酱</span>
-                                    <span>12:34</span>
-                                </div>
-                                <div>
-                                    Note that it's particularly not recommended to use a pixel dimension and an auto dimension with a gradient, because it's impossible to replicate rendering in versions of Firefox prior to 8, and in browsers not implementing Firefox 8's rendering, without knowing the exact size of the element whose background is being specified.
-                                </div>
-                            </div>
-                        </div>
-                        <div className="message message-other">
-                            <img src={ require('../image/avatar.gif') }/>
-                            <div>
-                                <div>
-                                    <span>碎碎酱</span>
-                                    <span>12:34</span>
-                                </div>
-                                <div>
-                                    以上现象是由于字体图标存在半个像素的锯齿，在浏览器渲染的时候直接显示一个像素了，导致在有背景下的图标显示感觉加粗；所以在应用字体图标的时候需要对图标样式进行抗锯齿处理，CSS代码设置如下：
-                                </div>
-                            </div>
-                        </div>
-                        <div className="message">
-                            <img src={ require('../image/avatar.gif') }/>
-                            <div>
-                                <div className="nick">
-                                    <span>碎碎酱</span>
-                                    <span>12:34</span>
-                                </div>
-                                <div>
-                                    Note that it's partie
-                                </div>
-                            </div>
-                        </div>
-                        <div className="message message-other">
-                            <img src={ require('../image/avatar.gif') }/>
-                            <div>
-                                <div>
-                                    <span>碎碎酱</span>
-                                    <span>12:34</span>
-                                </div>
-                                <div>
-                                    Note that it's partie
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <MessageList.container>
+                        <MessageList.item self/>
+                        <MessageList.item/>
+                        <MessageList.item/>
+                    </MessageList.container>
                     <div className="input"></div>
                 </div>
             </div>
---
+++ src/client/component/messageList.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import '../style/messageList.scss';
+
+class MessageList extends React.Component {
+    render () {
+        return (
+            <div className="message-list">
+                { this.props.children }
+            </div>
+        );
+    }
+}
+
+class Message extends React.Component {
+    render () {
+        let { self } = this.props;
+        return (
+            <div className={ `message-list-item ${ self ? 'message-self': '' }` }>
+                <img src={ require('../image/avatar.gif') }/>
+                <div>
+                    <div>
+                        <span>碎碎酱</span>
+                        <span>12:34</span>
+                    </div>
+                    <div>
+                        Note that it's particularly not recommended to use a pixel dimension and an auto dimension with a gradient, because it's impossible to replicate rendering in versions of Firefox prior to 8, and in browsers not implementing Firefox 8's rendering, without knowing the exact size of the element whose background is being specified.
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default {
+    container: MessageList,
+    item: Message,
+};
