d244594bba7ffc04cd47a71f99d2d578bac2d91f
碎碎酱 <yinxin630@gmail.com>
2016-07-31 22:02:58 +0800

    feat: add message

--- src/client/component/body.js
+++ src/client/component/body.js
@@ -11,7 +11,76 @@ class Body extends React.Component {
                     <UserList.item/>
                     <UserList.item/>
                 </UserList.container>
-                <div className="right"></div>
+                <div className="chatPanel">
+                    <div className="header">
+                        <div>
+                            <img src={ require('../image/avatar.gif') }/>
+                            <p>碎碎酱</p>
+                        </div>
+                        <div>
+                            <div>
+                                <i className="icon">&#xe603;</i>
+                            </div>
+                            <div>
+                                <i className="icon">&#xe603;</i>
+                            </div>
+                            <div>
+                                <i className="icon">&#xe603;</i>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="content">
+                        <div className="message">
+                            <img src={ require('../image/avatar.gif') }/>
+                            <div>
+                                <div>
+                                    <span>碎碎酱</span>
+                                    <span>12:34</span>
+                                </div>
+                                <div>
+                                    Note that it's particularly not recommended to use a pixel dimension and an auto dimension with a gradient, because it's impossible to replicate rendering in versions of Firefox prior to 8, and in browsers not implementing Firefox 8's rendering, without knowing the exact size of the element whose background is being specified.
+                                </div>
+                            </div>
+                        </div>
+                        <div className="message message-other">
+                            <img src={ require('../image/avatar.gif') }/>
+                            <div>
+                                <div>
+                                    <span>碎碎酱</span>
+                                    <span>12:34</span>
+                                </div>
+                                <div>
+                                    以上现象是由于字体图标存在半个像素的锯齿，在浏览器渲染的时候直接显示一个像素了，导致在有背景下的图标显示感觉加粗；所以在应用字体图标的时候需要对图标样式进行抗锯齿处理，CSS代码设置如下：
+                                </div>
+                            </div>
+                        </div>
+                        <div className="message">
+                            <img src={ require('../image/avatar.gif') }/>
+                            <div>
+                                <div className="nick">
+                                    <span>碎碎酱</span>
+                                    <span>12:34</span>
+                                </div>
+                                <div>
+                                    Note that it's partie
+                                </div>
+                            </div>
+                        </div>
+                        <div className="message message-other">
+                            <img src={ require('../image/avatar.gif') }/>
+                            <div>
+                                <div>
+                                    <span>碎碎酱</span>
+                                    <span>12:34</span>
+                                </div>
+                                <div>
+                                    Note that it's partie
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="input"></div>
+                </div>
             </div>
         );
     }
