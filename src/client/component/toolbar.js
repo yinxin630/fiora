fd379dbeabcc8d4fa861268cd274c30661d23eba
碎碎酱 <yinxin630@gmail.com>
2016-08-01 17:42:24 +0800

    feat: add toolbar show logic

--- src/client/component/toolbar.js
+++ src/client/component/toolbar.js
@@ -4,8 +4,17 @@ import '../style/toolbar.scss';
 import { connect } from 'react-redux';
 
 class Toolbar extends React.Component {
+    constructor (props) {
+        super(props);
+        this.renderToolbar = this.renderToolbar.bind(this);
+    }
+
     render () {
         let { show } = this.props;
+        return show ? this.renderToolbar() : null;
+    }
+    
+    renderToolbar () {
         return (
             <div className="toolbar">
                 <div>
