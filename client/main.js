67c894efa695c8d8b855ee40a8ba8617195cb41f
碎碎酱 <yinxin630@gmail.com>
2018-04-14 17:50:52 +0800

    feat: Supporting browser desktop notifications

--- client/main.js
+++ client/main.js
@@ -7,6 +7,11 @@ import App from './App';
 import store from './state/store';
 import action from './state/action';
 import socket from './socket';
+import notification from '../utils/notification';
+
+if (window.Notification && (window.Notification.permission === 'default' || window.Notification.permission === 'denied')) {
+    window.Notification.requestPermission();
+}
 
 socket.on('connect', () => {
     const token = window.localStorage.getItem('token');
@@ -41,6 +46,14 @@ socket.on('disconnect', () => {
 });
 socket.on('message', (message) => {
     action.addGroupMessage(message.toGroup, message);
+    const state = store.getState();
+    const group = state.getIn(['user', 'groups']).find(g => g.get('_id') === message.toGroup);
+    notification(
+        `${message.from.username} 在 ${group.get('name')} 对大家说:`,
+        message.from.avatar,
+        message.content,
+        message.toGroup._id,
+    );
 });
 
 ReactDom.render(
---
+++ utils/notification.js
@@ -0,0 +1,18 @@
+export default function notification(title, icon, body, tag = 'tag', duration = 3000) {
+    if (window.Notification && window.Notification.permission === 'granted') {
+        const n = new window.Notification(
+            title,
+            {
+                icon,
+                body,
+                tag,
+            },
+        );
+        n.onclick = function () {
+            window.blur();
+            window.focus();
+            this.close();
+        };
+        setTimeout(n.close.bind(n), duration);
+    }
+}
