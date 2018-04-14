cb580b709c2950995624d2d77935d84678e61693
碎碎酱 <yinxin630@gmail.com>
2018-04-14 21:41:44 +0800

    refactor: Refactoring the code to send pictures and paste the hair map

--- client/modules/main/chatPanel/chat/ChatInput.jsx
+++ client/modules/main/chatPanel/chat/ChatInput.jsx
@@ -15,6 +15,7 @@ import Message from '@/components/Message';
 import Expression from './Expression';
 import CodeEditor from './CodeEditor';
 import config from '../../../../../config/client';
+import readDiskFile from '../../../../../utils/readDiskFile';
 
 const xss = require('../../../../../utils/xss');
 
@@ -48,6 +49,17 @@ class ChatInput extends Component {
             input.value += value;
             input.focus();
         }
+    }
+    static compressImage(image, mimeType, quality = 1) {
+        return new Promise((resolve) => {
+            const canvas = document.createElement('canvas');
+            canvas.width = image.width;
+            canvas.height = image.height;
+
+            const ctx = canvas.getContext('2d');
+            ctx.drawImage(image, 0, 0);
+            canvas.toBlob(resolve, mimeType, quality);
+        });
     }
     constructor(...args) {
         super(...args);
@@ -154,56 +166,45 @@ class ChatInput extends Component {
         this.handleVisibleChange(false);
         ChatInput.insertAtCursor(this.message, `#(${expression})`);
     }
-    readFile(file) {
+    sendImageMessage(image) {
+        if (image.size > config.maxImageSize) {
+            return Message.warning('要发送的图片过大', 3);
+        }
+
         const { user, groupId: toGroup } = this.props;
-        const reader = new FileReader();
-        const that = this;
-        reader.onloadend = function () {
-            if (this.result.byteLength > config.maxImageSize) {
-                Message.warning('要发送的图片过大', 3);
-                return;
-            }
+        const ext = image.type.split('/').pop().toLowerCase();
+        const url = URL.createObjectURL(image);
 
-            const ext = file.name.split('.').pop().toLowerCase();
-            const blob = new Blob([new Uint8Array(this.result)], { type: `image/${ext}` });
-            const url = URL.createObjectURL(blob);
-
-            const img = new Image();
-            img.onload = () => {
-                const id = that.addSelfMessage('image', `${url}?width=${img.width}&height=${img.height}`);
-
-                socket.emit('uploadToken', {}, (res) => {
-                    if (typeof token === 'string') {
-                        Message.error(res);
-                    } else {
-                        const result = qiniu.upload(blob, `ImageMessage/${user.get('_id')}_${Date.now()}.${ext}`, res.token, { useCdnDomain: true }, {});
-                        result.subscribe({
-                            next(info) {
-                                action.updateSelfMessage(toGroup, id, { percent: info.total.percent });
-                            },
-                            error(err) {
-                                console.error(err);
-                                Message.error('上传图片失败');
-                            },
-                            complete(info) {
-                                const imageUrl = `${res.urlPrefix + info.key}?width=${img.width}&height=${img.height}`;
-                                that.sendMessage(id, 'image', imageUrl);
-                            },
-                        });
-                    }
-                });
-            };
-            img.src = url;
+        const img = new Image();
+        img.onload = () => {
+            const id = this.addSelfMessage('image', `${url}?width=${img.width}&height=${img.height}`);
+            socket.emit('uploadToken', {}, (res) => {
+                if (typeof token === 'string') {
+                    Message.error(res);
+                } else {
+                    const result = qiniu.upload(image, `ImageMessage/${user.get('_id')}_${Date.now()}.${ext}`, res.token, { useCdnDomain: true }, {});
+                    result.subscribe({
+                        next(info) {
+                            action.updateSelfMessage(toGroup, id, { percent: info.total.percent });
+                        },
+                        error(err) {
+                            console.error(err);
+                            Message.error('上传图片失败');
+                        },
+                        complete: (info) => {
+                            const imageUrl = `${res.urlPrefix + info.key}?width=${img.width}&height=${img.height}`;
+                            this.sendMessage(id, 'image', imageUrl);
+                        },
+                    });
+                }
+            });
         };
-        reader.readAsArrayBuffer(file);
+        img.src = url;
     }
     @autobind
-    handleSelectFile() {
-        const image = this.file.files[0];
-        if (!image) {
-            return;
-        }
-        this.readFile(image);
+    async handleSelectFile() {
+        const image = await readDiskFile('blob', 'image/png,image/jpeg,image/gif');
+        this.sendImageMessage(image.result);
     }
     @autobind
     handlePaste(e) {
@@ -217,9 +218,17 @@ class ChatInput extends Component {
                 if (item.kind === 'file') {
                     const file = item.getAsFile();
                     if (file) {
-                        console.log(file.name, file);
-                        console.log(this);
-                        this.readFile(file);
+                        const that = this;
+                        const reader = new FileReader();
+                        reader.onloadend = function () {
+                            const image = new Image();
+                            image.onload = async () => {
+                                const imageBlob = await ChatInput.compressImage(image, file.type, 0.8);
+                                that.sendImageMessage(imageBlob);
+                            };
+                            image.src = this.result;
+                        };
+                        reader.readAsDataURL(file);
                     }
                 }
             }
