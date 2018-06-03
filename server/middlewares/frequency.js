a4a14c25486372b10bd96d0ab82c7643231bcda9
碎碎酱 <yinxin630@gmail.com>
2018-06-03 22:37:28 +0800

    feat: Change call interface frequency

--- server/middlewares/frequency.js
+++ server/middlewares/frequency.js
@@ -1,4 +1,4 @@
-const MaxCallPerMinutes = 60;
+const MaxCallPerMinutes = 30;
 /**
  * Limiting the frequency of interface calls
  */
@@ -7,6 +7,11 @@ module.exports = function () {
     setInterval(() => callTimes = {}, 60000); // Emptying every 60 seconds
 
     return async (ctx, next) => {
+        // robot10
+        if (ctx.socket.user === '5adad39555703565e7903f79') {
+            return next();
+        }
+
         const socketId = ctx.socket.id;
         const count = callTimes[socketId] || 0;
         if (count >= MaxCallPerMinutes) {
