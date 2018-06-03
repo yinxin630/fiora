a08f4a470035c66f7fa6b79bd327dad5ba0cebc9
碎碎酱 <yinxin630@gmail.com>
2018-06-03 22:46:46 +0800

    feat: Controlling the total number of messages

--- client/state/reducer.js
+++ client/state/reducer.js
@@ -168,9 +168,13 @@ function reducer(state = initialState, action) {
                 linkmans
                     .delete(linkmanIndex)
                     .unshift(linkman
-                        .update('messages', messages => (
-                            messages.push(immutable.fromJS(action.message))
-                        ))
+                        .update('messages', (messages) => {
+                            const newMessages = messages.push(immutable.fromJS(action.message));
+                            if (newMessages.size > 300) {
+                                return newMessages.splice(0, 200);
+                            }
+                            return newMessages;
+                        })
                         .set('unread', unread))
             ));
     }
