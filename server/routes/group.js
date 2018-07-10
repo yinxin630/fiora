41073034984f7a1bd34150be09d028add7227d05
碎碎酱 <yinxin630@gmail.com>
2018-07-10 21:46:52 +0800

    fix: Solve the problem of exiting the default group error

--- server/routes/group.js
+++ server/routes/group.js
@@ -96,7 +96,11 @@ module.exports = {
 
         const group = await Group.findOne({ _id: groupId });
         assert(group, '群组不存在');
-        assert(group.creator.toString() !== ctx.socket.user.toString(), '群主不可以退出自己创建的群');
+
+        // 默认群组没有creator
+        if (group.creator) {
+            assert(group.creator.toString() !== ctx.socket.user.toString(), '群主不可以退出自己创建的群');
+        }
 
         const index = group.members.indexOf(ctx.socket.user);
         assert(index !== -1, '你不在群组中');
