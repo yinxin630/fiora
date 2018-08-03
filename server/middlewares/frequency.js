933fa826c4af7d57a0ae74124da0cf6fb0714ab6
碎碎酱 <yinxin630@gmail.com>
2018-08-03 09:40:00 +0800

    feat: New user restrictions modify text info

--- server/middlewares/frequency.js
+++ server/middlewares/frequency.js
@@ -16,8 +16,14 @@ module.exports = function () {
         const newUserList = global.mdb.get('newUserList');
         const socketId = ctx.socket.id;
         const count = callTimes[socketId] || 0;
-        if (count >= (user && newUserList.has(user.toString()) ? 5 : MaxCallPerMinutes)) {
-            return ctx.res = '接口调用频繁';
+
+        // 萌新限制
+        if (user && newUserList.has(user.toString()) && count > 5) {
+            return ctx.res = '接口调用失败, 你正处于萌新限制期, 请不要频繁操作';
+        }
+        // 普通用户限制
+        if (count > MaxCallPerMinutes) {
+            return ctx.res = '接口调用频繁, 请稍后再试';
         }
         callTimes[socketId] = count + 1;
         await next();
