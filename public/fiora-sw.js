6730b5b14477e486615966b6b3345f512ebb2e5d
碎碎酱 <yinxin630@gmail.com>
2018-08-16 22:45:18 +0800

    refactor: Compatible ES5

--- public/fiora-sw.js
+++ public/fiora-sw.js
@@ -1,27 +1,13 @@
 importScripts('https://g.alicdn.com/kg/workbox/3.3.0/workbox-sw.js');
 
 if (workbox) {
-    workbox.setConfig({
-        modulePathPrefix: 'https://g.alicdn.com/kg/workbox/3.3.0/',
-    });
+    workbox.setConfig({ modulePathPrefix: 'https://g.alicdn.com/kg/workbox/3.3.0/' });
 
-    workbox.precaching.precache([
-        '/',
-        '/index.html',
-    ]);
+    workbox.precaching.precache(['/', '/index.html']);
 
-    workbox.routing.registerRoute(
-        new RegExp('.*.html'),
-        workbox.strategies.networkFirst(),
-    );
+    workbox.routing.registerRoute(new RegExp('.*.html'), workbox.strategies.networkFirst());
 
-    workbox.routing.registerRoute(
-        new RegExp('.*.(?:js|css)'),
-        workbox.strategies.staleWhileRevalidate(),
-    );
+    workbox.routing.registerRoute(new RegExp('.*.(?:js|css)'), workbox.strategies.staleWhileRevalidate());
 
-    workbox.routing.registerRoute(
-        new RegExp('https://cdn.suisuijiang.com/'),
-        workbox.strategies.cacheFirst(),
-    );
+    workbox.routing.registerRoute(new RegExp('https://cdn.suisuijiang.com/'), workbox.strategies.cacheFirst());
 }
