0c350b73629adbd24650e5f3bc89315c72818884
碎碎酱 <yinxin630@gmail.com>
2018-08-30 22:43:28 +0800

    feat: Update service worker

--- public/fiora-sw.js
+++ public/fiora-sw.js
@@ -5,7 +5,7 @@ if (workbox) {
 
     workbox.precaching.precache(['/', '/index.html']);
 
-    workbox.routing.registerRoute(new RegExp('^https?://fiora.suisuijiang.com$'), workbox.strategies.networkFirst());
+    workbox.routing.registerRoute(new RegExp('^https?://fiora.suisuijiang.com/?$'), workbox.strategies.networkFirst());
 
     workbox.routing.registerRoute(new RegExp('.*.html'), workbox.strategies.networkFirst());
 
