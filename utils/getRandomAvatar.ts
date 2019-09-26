7619181cd4d31aae1cf38f42e04d69a89de1c5aa
碎碎酱 <yinxin630@gmail.com>
2019-09-26 21:59:02 +0800

    fix: Randow avatar wrong path
    close #141

--- utils/getRandomAvatar.ts
+++ utils/getRandomAvatar.ts
@@ -2,19 +2,19 @@ import webpackConfig from '../config/webpack';
 
 const AvatarCount = 15;
 const env = process.env.NODE_ENV === 'development' ? 'dev' : 'build';
-const publishPath = webpackConfig[env].assetsPublicPath;
+const publicPath: string = webpackConfig[env].assetsPublicPath + (webpackConfig[env].assetsPublicPath.endsWith('/') ? '' : '/');
 
 /**
  * 获取随机头像
  */
 export default function getRandomAvatar() {
     const number = Math.floor(Math.random() * AvatarCount);
-    return `${publishPath}avatar/${number}.jpg`;
+    return `${publicPath}avatar/${number}.jpg`;
 }
 
 /**
  * 获取默认头像
  */
 export function getDefaultAvatar() {
-    return `${publishPath}avatar/0.jpg`;
+    return `${publicPath}avatar/0.jpg`;
 }
