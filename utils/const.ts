128f67d7d640191ea73f2aae57d99e8d64c8915d
碎碎酱 <yinxin630@gmail.com>
2020-02-07 16:33:55 +0800

    feat: modify seal timeout

--- utils/const.ts
+++ utils/const.ts
@@ -2,10 +2,10 @@
 export const SealText = '你已经被关进小黑屋中, 请反思后再试';
 
 /** 封禁用户释放时间 */
-export const SealUserTimeout = 1000 * 60 * 10;
+export const SealUserTimeout = 1000 * 60 * 10; // 10分钟
 
 /** 封禁ip释放时间 */
-export const SealIpTimeout = 1000 * 60 * 1; // 先设置为一分钟, 线上试试效果
+export const SealIpTimeout = 1000 * 60 * 60 * 6; // 6小时
 
 /** 透明图 */
 export const transparentImage = 'data:image/png;base64,R0lGODlhFAAUAIAAAP///wAAACH5BAEAAAAALAAAAAAUABQAAAIRhI+py+0Po5y02ouz3rz7rxUAOw==';
