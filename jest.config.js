593f80772238f12ad09569108b40abdc472fefc6
碎碎酱 <yinxin630@gmail.com>
2020-07-28 17:56:20 +0800

    chore: disable typescript type check during run unit test

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,13 @@
 module.exports = {
     roots: ['./test'],
+    preset: 'ts-jest',
     transform: {
-        '^.+\\.tsx?$': 'ts-jest',
         '\\.(less|jpg)$': '<rootDir>/jest.transformer.js',
     },
     collectCoverage: true,
+    globals: {
+        'ts-jest': {
+            isolatedModules: true,
+        },
+    },
 };
