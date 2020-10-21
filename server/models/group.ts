7e01bf85274cbe42c71d73a2ba02ce5ca9cc306d
碎碎酱 <yinxin630@gmail.com>
2020-10-21 16:59:03 +0800

    feat: increase group name max length to 10

--- server/models/group.ts
+++ server/models/group.ts
@@ -8,7 +8,7 @@ const GroupSchema = new Schema({
         type: String,
         trim: true,
         unique: true,
-        match: /^([0-9a-zA-Z]{1,2}|[\u4e00-\u9eff]){1,8}$/,
+        match: /^([0-9a-zA-Z]{1,2}|[\u4e00-\u9eff]){1,10}$/,
         index: true,
     },
     avatar: String,
