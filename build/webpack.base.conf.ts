fbdd44d3caea64d28e2711e0923ea64926d47d2e
碎碎酱 <yinxin630@gmail.com>
2020-11-03 21:22:32 +0800

    chore: enable ts-loader transpileOnly when in production env

--- build/webpack.base.conf.ts
+++ build/webpack.base.conf.ts
@@ -47,7 +47,7 @@ export default {
                     {
                         loader: 'ts-loader',
                         options: {
-                            transpileOnly: true,
+                            transpileOnly: process.env.NODE_ENV === 'production',
                         },
                     },
                 ],
