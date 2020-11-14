5f93cf885c5ab7aa687a08706ea1e16d62544674
碎碎酱 <yinxin630@gmail.com>
2020-11-14 23:36:12 +0800

    chore: use webpack plugin to generate service worker script

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge');
 const TerserPlugin = require('terser-webpack-plugin');
 const ScriptExtHtmlPlugin = require('script-ext-html-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
@@ -31,6 +32,10 @@ module.exports = merge(common, {
                     value: 'anonymous',
                 },
             ],
+        }),
+        new WorkboxPlugin.GenerateSW({
+            clientsClaim: true,
+            skipWaiting: true,
         }),
     ],
 });
