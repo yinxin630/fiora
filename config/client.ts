bcf4dd031e71c2a33e0f34f2d1cbcba3a7459ca0
碎碎酱 <yinxin630@gmail.com>
2020-11-14 23:09:13 +0800

    feat: adapt to ios full screen devices

--- config/client.ts
+++ config/client.ts
@@ -2,7 +2,7 @@ import { MB } from '../utils/const';
 
 export default {
     server:
-        process.env.Server || (process.env.NODE_ENV === 'development' ? '//localhost:9200' : '/'),
+        process.env.Server || (process.env.NODE_ENV === 'development' ? '//192.168.124.16:9200' : '/'),
 
     maxImageSize: process.env.MaxImageSize ? parseInt(process.env.MaxImageSize, 10) : MB * 3,
     maxBackgroundImageSize: process.env.MaxBackgroundImageSize
