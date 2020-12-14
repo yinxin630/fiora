20e30e420a0821d5495d6672413fa47994607f12
碎碎酱 <yinxin630@gmail.com>
2020-12-14 22:29:37 +0800

    feat: add new script of delete messages

---
+++ bin/deleteMessages.ts
@@ -0,0 +1,49 @@
+import path from 'path';
+import fs from 'fs';
+import inquirer from 'inquirer';
+import { promisify } from 'util';
+import chalk from 'chalk';
+import connectDB from '../server/mongoose';
+import Message from '../server/models/message';
+
+export async function deleteMessages() {
+    const shouldDeleteAllMessages = await inquirer.prompt({
+        type: 'confirm',
+        name: 'result',
+        message: 'Confirm to delete all messages?',
+    });
+    if (!shouldDeleteAllMessages.result) {
+        return;
+    }
+
+    await connectDB();
+
+    const deleteResult = await Message.deleteMany({});
+    console.log('Delete result:', deleteResult);
+
+    const shouldDeleteAllFiles = await inquirer.prompt({
+        type: 'confirm',
+        name: 'result',
+        message: 'Confirm to delete all message files?',
+    });
+    if (!shouldDeleteAllFiles.result) {
+        return;
+    }
+
+    const files = await promisify(fs.readdir)(path.resolve(__dirname, '../public'));
+    const iamgesAndFiles = files.filter(
+        (filename) => filename.startsWith('ImageMessage_') || filename.startsWith('FileMessage_'),
+    );
+    const unlinkAsync = promisify(fs.unlink);
+    await Promise.all(
+        iamgesAndFiles.map((file) => unlinkAsync(path.resolve(__dirname, '../public/', file))),
+    );
+    console.log('Delete files:');
+    console.log(chalk.red(iamgesAndFiles.join('\n')));
+}
+
+async function run() {
+    await deleteMessages();
+    process.exit(0);
+}
+export default run;
