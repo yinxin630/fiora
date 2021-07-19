cedcb63da0b7a8daa881f41387b61cb7eb39f258
碎碎酱 <yinxin630@gmail.com>
2021-07-19 22:31:40 +0800

    Update iOS app download url and QRCode images

--- packages/web/src/modules/Sidebar/Download.tsx
+++ packages/web/src/modules/Sidebar/Download.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import QRCode from 'qrcode.react';
 
 import Dialog from '../../components/Dialog';
-
 import Style from './Download.less';
 import Common from './Common.less';
 
@@ -12,6 +12,9 @@ interface DownloadProps {
 
 function Download(props: DownloadProps) {
     const { visible, onClose } = props;
+    const androidDownloadUrl = `${window.location.origin}/fiora.apk`;
+    const iOSDownloadUrl = 'https://apps.apple.com/cn/app/fiora/id1554719127';
+
     return (
         <Dialog className={Style.download} visible={visible} title="下载APP" onClose={onClose}>
             <div className={Common.container}>
@@ -19,17 +22,18 @@ function Download(props: DownloadProps) {
                     <p className={Common.title}>Android</p>
                     <div className={Style.android}>
                         <p>
-                            链接:{' '}
-                            <a href="/fiora.apk">
-                                {`${window.location.origin}/fiora.apk`}
-                            </a>
+                            链接: <a href={androidDownloadUrl}>{androidDownloadUrl}</a>
                         </p>
+                        <QRCode value={androidDownloadUrl} size={200} />
                     </div>
                 </div>
                 <div className={Common.block}>
                     <p className={Common.title}>iOS</p>
                     <div className={Style.ios}>
-                        <p>审核中, 敬请期待~~</p>
+                        <p>
+                            链接: <a href={iOSDownloadUrl}>{iOSDownloadUrl}</a>
+                        </p>
+                        <QRCode value={iOSDownloadUrl} size={200} />
                     </div>
                 </div>
             </div>
